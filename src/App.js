94e1c6a2586f704246f2a48f025f0f5da7e938ac
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-06-22 20:24:14 +0200

    first commit after install app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
